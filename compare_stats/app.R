0d052da47ee0046ba80ec27f48ad875f401a718f
Logan Godfrey <logan.godfrey@duke.edu>
2024-04-02 22:34:29 -0400

    Added data for 2017-23 seasons, fixed app so everything should work now. Going to change so it auto-updates but this will work for now.

--- compare_stats/app.R
+++ compare_stats/app.R
@@ -1,4 +1,11 @@
 library(shiny)
+library(tidyverse)
+library(hoopR)  
+library(tidymodels)
+
+filter_choices <- c("Clutch", "FG_PCT", "PTS", "PF", "BLK", "STL", "TOV", "AST", "DREB", "OREB")
+
+year <- c("1996-97", "1997-98", "1998-99", "1999-00", "2000-01", "2001-02", "2002-03", "2003-04", "2004-05", "2005-06", "2006-07", "2007-08", "2008-09", "2009-10", "2010-11", "2011-12", "2012-13", "2013-14", "2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2010-20", "2020-21", "2021-22", "2022-23", "2023-24")
 
 ui <- fluidPage(
   
@@ -6,10 +13,10 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("player", "Player Selection:",
-                  player_names, multiple = T),
       selectInput("filter", "Filter Data",
-                  filter_choices)
+                  filter_choices),
+      selectInput("year", "Choose Year",
+                  year)
     ),
     mainPanel(
       tableOutput("player_clutch")
@@ -19,8 +26,7 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  output$player_clutch <- player_final |> 
-    filter(PLAYER_NAME %in% input$player) |> 
+   output$player_clutch <- read.csv(paste("clutch", gsub(pattern = "-.*", replacement = "", x = input$year), ".csv", sep = "")) |> 
     select(PLAYER_NAME, clutch_score, TEAM_ABBREVIATION, W, L, FG_PCT,
            PTS, PF, BLK, STL, TOV, AST, DREB, OREB) |> 
     rename(TEAM = TEAM_ABBREVIATION,
