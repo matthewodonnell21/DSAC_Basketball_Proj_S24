5074a31434edf34ad249a693a8aa25eb739d4f27
Logan Godfrey <logan.godfrey@duke.edu>
2024-04-08 03:52:26 -0400

    Finished compare app. Everything should be working now (although it's not the fastest app ever when looking at all-time data). Feel free to make suggestions on things that could be changed to improve the app.

--- compare_stats/compare2.R
+++ compare_stats/compare2.R
@@ -4,90 +4,147 @@ library(hoopR)
 library(tidymodels)
 library(shinythemes)
 
-filter_choices <- c("Clutch", "FG_PCT", "PTS", "PF", "BLK", "STL", "TOV", "AST", "DREB", "OREB")
+filter_choices <- c("Name", "Season", "Team", "Clutch", "PA.PER.MIN", "MIN", 
+                    "PTS", "AST", "REB", "BLK", "STL", "TOV", "FG_PCT", "FG3_PCT", "FT_PCT")
 
-year <- c("1996-97", "1997-98", "1998-99", "1999-00", "2000-01", "2001-02", "2002-03", "2003-04", "2004-05", "2005-06", "2006-07", "2007-08", "2008-09", "2009-10", "2010-11", "2011-12", "2012-13", "2013-14", "2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24")
+year <- c("1996-97", "1997-98", "1998-99", "1999-00", "2000-01", "2001-02", "2002-03", "2003-04", "2004-05", "2005-06", "2006-07", "2007-08", "2008-09", "2009-10", "2010-11", "2011-12", "2012-13", "2013-14", "2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "All")
 
 excluded <- c("GROUP_SET", "PLAYER_NAME", "NICKNAME", "TEAM_ABBREVIATION")
 
+clutch1 <- function(year) {
+  
+  d2 <- read.csv(paste("data/_", gsub(pattern = "-.*", replacement = "", x = year), ".csv", sep = ""))
+  
+  weights <- lm(W_PCT ~ FG_PCT + PTS + PF + BLK + STL + TOV + AST + DREB + OREB, data = d2) |> 
+    tidy() |> 
+    slice(2:10) |> 
+    select(estimate) |>
+    data.matrix()
+  
+  final <- d2 |> 
+    select(FG_PCT, PTS, PF, BLK, STL, TOV, AST, DREB, OREB) |> 
+    data.matrix() %*% weights |>
+    as.data.frame() |>
+    cbind(d2) |> 
+    rename(clutch_score = estimate)
+  
+  return(final)
+}
+
+points.added.leaderboard = function(year){
+  
+  if(year == "All"){
+    ret = NULL
+    for(i in 1996:2023){
+      cur = points.added.leaderboard(i)
+      
+      ret = rbind(ret, cur)
+    }
+    ret = ret %>% select(Season, PLAYER_ID, PA.PER.MIN)
+    
+    return(ret)
+    
+  }
+  
+  file_path = paste("data/_", gsub(pattern = "-.*", replacement = "", x = year), ".csv", sep = "")
+  season_totals <- read.csv(file_path)
+  season_totals <- season_totals |> 
+    mutate(Season = gsub(pattern = "-.*", replacement = "", x = year)) |> 
+    mutate(Season = as.numeric(Season))
+  
+  season_ratings = read.csv("data/per_possession_data.csv") %>% 
+    filter(Season == gsub(pattern = "-.*", replacement = "", x = year))
+  
+  ppp = season_ratings$ppp
+  orb.pct = season_ratings$orb.pct
+  drb.pct = 1 - orb.pct
+  ppFGM = season_ratings$ppFGM
+  
+  PA_2FGM = 2 - ppp
+  PA_3FGM = 3 - ppp
+  PA_FTM = 1 - (ppp*0.44)
+  PA_FG.miss = -drb.pct*ppp
+  PA_FT.miss = -0.44*ppp
+  PA_assist = ppFGM - ppp
+  PA_drb = orb.pct*ppp
+  PA_orb = drb.pct*ppp
+  PA_stls = ppp
+  PA_blk = ppp*drb.pct
+  PA_TO = -ppp
+  
+  season_totals = season_totals %>% mutate(TOTAL.PA = PA_2FGM*(FGM-FG3M) +
+                                             PA_3FGM*FG3M + 
+                                             PA_FTM*FTM + 
+                                             PA_FG.miss*(FGA-FGM) + 
+                                             PA_FT.miss*(FTA - FTM) +
+                                             PA_assist*AST + 
+                                             PA_drb*DREB +
+                                             PA_orb*OREB + 
+                                             PA_stls*STL +
+                                             PA_blk*BLK +
+                                             PA_TO*TOV,
+                                           PA.PER.MIN = TOTAL.PA / MIN)
+  
+  season_totals = season_totals %>% 
+    select(Season, PLAYER_ID, PA.PER.MIN) |> 
+    mutate(PLAYER_ID = as.numeric(PLAYER_ID))
+  
+  return(season_totals)
+}
+
 ui <- fluidPage(theme = shinytheme("darkly"),
-  
-  titlePanel("Clutch Players"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("filter", "Filter Data",
-                  filter_choices),
-      selectInput("year", "Choose Year",
-                  year),
-      numericInput("minutes", "Choose Minute Minimum", 0)
-    ),
-    mainPanel(
-      tableOutput("player_clutch")
-    )
-  )
+                
+                titlePanel("Clutch Players"),
+                
+                sidebarLayout(
+                  sidebarPanel(
+                    selectInput("filter", "Filter Data",
+                                filter_choices),
+                    selectInput("year", "Choose Year",
+                                year),
+                    numericInput("minutes", "Choose Minute Minimum", 0)
+                  ),
+                  mainPanel(
+                    tableOutput("player_clutch")
+                  )
+                )
 )
 
 server <- function(input, output) {
   
   output$player_clutch <- renderTable({
-  
-    if(year == "ALL"){
-      ret = NULL
-      for(i in 1996:2023){
-        cur = points.added.leaderboard(i, min.played) %>% 
+    
+    if(input$year == "All"){
+      final_list <- lapply(1996:2023, function(i) {
+        current <- clutch1(i) |> 
           mutate(Season = i)
-        ret = rbind(ret, cur)
-      }
-      ret = ret %>% select(Season, PLAYER_ID, PLAYER_NAME, 
-                           TEAM_ABBREVIATION, MIN, TOTAL.PA, PA.PER.MIN, PTS, 
-                           AST, REB, BLK, STL, TOV, FG_PCT, FG3_PCT, FT_PCT)
-      
-      return(ret)
-      
+        return(current)
+      })
+      final <- bind_rows(final_list)
+    } else {
+      final <- clutch1(input$year) |> 
+        mutate(Season = gsub(pattern = "-.*", replacement = "", x = input$year))
     }
     
-    if(!(input$year %in% 2002:2023)){
+    leaderboard_data <- points.added.leaderboard(gsub(pattern = "-.*", replacement = "", x = input$year))
     
-   d1 <- nba_leaguedashplayerclutch(season = year_to_season(gsub(pattern = "-.*", replacement = "", x = input$year))) |> 
-                    as.data.frame() |> 
-                    rename_with(.cols = everything(), .fn = ~gsub(pattern = "LeagueDashPlayerClutch.", replacement = "", x = .)) |> 
-                    mutate_if(is.character, as.numeric) |> 
-                    select(-excluded)
-   
-   d2 <- nba_leaguedashplayerclutch(season = year_to_season(gsub(pattern = "-.*", replacement = "", x = input$year))) |>
-                    as.data.frame() |> 
-                    rename_with(.cols = everything(), .fn = ~gsub(pattern = "LeagueDashPlayerClutch.", replacement = "", x = .)) |> 
-                    mutate(PLAYER_ID = as.numeric(PLAYER_ID)) |>
-                    select(PLAYER_ID, PLAYER_NAME, NICKNAME, TEAM_ABBREVIATION) |>
-                    left_join(d1, join_by(PLAYER_ID))
-   
-   weights <- reactive(lm(W_PCT ~ FG_PCT + PTS + PF + BLK + STL + TOV + AST + DREB + OREB, data = d2) |> 
-                         tidy() |> 
-                         slice(2:10) |> 
-                         select(estimate) |> 
-                         data.matrix())
-   
-   final <- data.matrix(select(d2, FG_PCT, PTS, PF, BLK, STL, TOV, AST, DREB, OREB)) %*% weights() |>
-     as.data.frame() |> 
-     cbind(d2) |> 
-     rename(clutch_score = estimate)
-     
-    }
-   
-     final_data <- data.frame(final |> 
-       filter(MIN > input$minutes) |> 
-       select(PLAYER_NAME, clutch_score, TEAM_ABBREVIATION, W, L, FG_PCT,
-                                  PTS, PF, BLK, STL, TOV, AST, DREB, OREB) %>%
-       rename(TEAM = TEAM_ABBREVIATION,
-              Name = PLAYER_NAME,
-              Clutch = clutch_score) %>%
-       mutate(Clutch = Clutch * 100) %>%
-       arrange(desc(!!sym(input$filter))))
-     
-     final_data
-       
-   })
+    final <- data.frame(final) %>%
+      select(Season, MIN, PLAYER_ID, PLAYER_NAME, clutch_score, TEAM_ABBREVIATION, W, L, FG_PCT,
+             PTS, PF, BLK, STL, TOV, AST, DREB, OREB) %>%
+      mutate(Season = as.numeric(Season)) |> 
+      rename(Team = TEAM_ABBREVIATION,
+             Name = PLAYER_NAME,
+             Clutch = clutch_score) %>%
+      mutate(Clutch = Clutch * 100) %>%
+      left_join(leaderboard_data, by = c("PLAYER_ID", "Season")) |> 
+      filter(MIN > input$minutes) %>%
+      arrange(desc(!!sym(input$filter))) |> 
+      select(Name, Season, Team, Clutch, PA.PER.MIN, MIN, 
+             PTS, AST, OREB, DREB, BLK, STL, TOV, FG_PCT)
+    
+    final
+    
+  })
   
 }
 
